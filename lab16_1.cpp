b671651d6027e5e3dbccf1cb9213b483856d45d8
PoommipatWat <poommipat_wat@cmu.ac.th>
2022-01-13 15:28:20 +0700

    "lab16_1.cpp completed"

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -1,9 +1,22 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
-
-	
-	
+	int a = 5;
+	int b = 'A';
+	int &c = b;
+	int *x = &a;
+	int *y = &b;
+	int **z = &x;
+	cout << a << ' ' << char(b) << ' ' << char(c) << ' ' << x << ' ' << y << ' ' << z << endl;
+	cout << &a << ' ' << &b << ' ' << &c << ' ' << &x << ' ' << &y << ' ' << &z << endl;
+	c = 'F';
+	cout << a << ' ' << char(b) << ' ' << char(c) << ' ' << x << ' ' << y << ' ' << z << endl;
+	*y = 'W';
+	cout << a << ' ' << char(b) << ' ' << char(c) << ' ' << x << ' ' << y << ' ' << z << endl;
+	*x = 6;
+	cout << a << ' ' << char(b) << ' ' << char(c) << ' ' << x << ' ' << y << ' ' << z << endl;
+	**z = 7;
+	cout << a << ' ' << char(b) << ' ' << char(c) << ' ' << x << ' ' << y << ' ' << z << endl;
 	return 0;
 }
